0daf2ae6fb1392763404b6ae76e8360310b33ac4
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-20 23:53:23 +0000

    feat: add optional callback props for header actions

--- lib/header.dart
+++ lib/header.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomHeader extends StatelessWidget {
-  const CustomHeader({super.key});
+  final VoidCallback? onHomePressed;
+  final VoidCallback? onAboutPressed;
+  final VoidCallback? onSearchPressed;
+  final VoidCallback? onProfilePressed;
+  final VoidCallback? onCartPressed;
+  final VoidCallback? onMenuPressed;
+
+  const CustomHeader({
+    super.key,
+    this.onHomePressed,
+    this.onAboutPressed,
+    this.onSearchPressed,
+    this.onProfilePressed,
+    this.onCartPressed,
+    this.onMenuPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
