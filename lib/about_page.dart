0f1c2b0bd9281e5d89173b45eedbe26b74c399cd
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-20 23:03:17 +0000

    feat: add initial content to About Us page

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -11,8 +11,35 @@ class AboutPage extends StatelessWidget {
         backgroundColor: const Color(0xFF4d2963),
         foregroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text('About Us'),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome to the Union Shop!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 24),
+              Text(
+                'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
+                'All online purchases are available for delivery or instore collection!\n\n'
+                'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
+                'Happy shopping!\n\n'
+                'The Union Shop & Reception Team',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.5,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
