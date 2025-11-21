87a8b0ea928619bc2a86edee30c91959aceb6960
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-21 02:24:10 +0000

    feat: add Footer component to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
 import 'package:union_shop/about_page.dart';
 import 'package:union_shop/header.dart';
+import 'package:union_shop/footer.dart'; // Add this import
 
 void main() {
   runApp(const UnionShopApp());
@@ -175,20 +176,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
 
-            // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            const Footer(),
           ],
         ),
       ),
