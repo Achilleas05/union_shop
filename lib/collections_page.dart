4417ce36ef290017bfcd146ceb186b980cae6dcf
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-21 03:20:40 +0000

    feat: create empty CollectionsPage with basic imports

---
+++ lib/collections_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/footer.dart';
+import 'package:union_shop/header.dart';
+
+class CollectionsPage extends StatelessWidget {
+  const CollectionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
