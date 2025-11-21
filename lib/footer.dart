0ad1a6ee3517dffce9661b91a16b023147a6effa
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-21 01:12:38 +0000

    feat: create basic Footer widget structure

---
+++ lib/footer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.grey[50],
+      padding: const EdgeInsets.all(40),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
