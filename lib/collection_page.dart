483633a52f688bb8a2fe034354db0aac2b51f89e
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-11-25 02:07:58 +0000

    refactor: replace hardcoded products with dynamic collectionProducts

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -134,7 +134,7 @@ class _CollectionPageState extends State<CollectionPage> {
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
           childAspectRatio: 0.75,
-          children: products.map(_buildProductCard).toList(),
+          children: widget.collectionProducts.map(_buildProductCard).toList(),
         ),
       );
 
@@ -242,66 +242,3 @@ class _CollectionPageState extends State<CollectionPage> {
         ],
       );
 }
-
-class Product {
-  final String id;
-  final String name;
-  final String imageUrl;
-  final double price;
-  final double? originalPrice;
-  final String? tag;
-
-  const Product({
-    required this.id,
-    required this.name,
-    required this.imageUrl,
-    required this.price,
-    this.originalPrice,
-    this.tag,
-  });
-}
-
-const List<Product> products = [
-  Product(
-    id: 'hoodie-1',
-    name: 'Union Autumn Hoodie',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 34.99,
-    originalPrice: 44.99,
-    tag: 'Sale',
-  ),
-  Product(
-    id: 'tshirt-1',
-    name: 'Autumn Leaves T-Shirt',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 19.99,
-  ),
-  Product(
-    id: 'beanie-1',
-    name: 'Wool Beanie',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 12.99,
-    tag: 'New',
-  ),
-  Product(
-    id: 'scarf-1',
-    name: 'Knit Scarf',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 24.99,
-    originalPrice: 29.99,
-    tag: 'Sale',
-  ),
-  Product(
-    id: 'mug-1',
-    name: 'Autumn Mug',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 8.99,
-  ),
-  Product(
-    id: 'socks-1',
-    name: 'Wool Socks',
-    imageUrl: 'assets/portsmouth_hoodie.png',
-    price: 6.99,
-    tag: 'New',
-  ),
-];
