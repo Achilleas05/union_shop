383cd399725e290d4a21fd4a25c0323bfe6e2079
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-12-02 14:18:10 +0000

    feat: expand product category mapping to include new items

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -27,12 +27,49 @@ class Product {
 
   // A category mapping based on id or name.
   String get category {
+    // Clothing
     if (id.startsWith('hoodie') || name.contains('Hoodie')) return 'Clothing';
+    if (id.startsWith('polo') || name.contains('Polo')) return 'Clothing';
+    if (id.startsWith('pants') || name.contains('Pants')) return 'Clothing';
+    if (id.startsWith('varsity') || name.contains('Varsity')) return 'Clothing';
+
+    // Accessories
     if (id.startsWith('beanie') || name.contains('Beanie')) {
       return 'Accessories';
     }
     if (id.startsWith('bag') || name.contains('Bag')) return 'Accessories';
+    if (id.startsWith('keychain') || name.contains('Keychain')) {
+      return 'Accessories';
+    }
+    if (id.startsWith('grad-hat') || name.contains('Graduation Cap')) {
+      return 'Accessories';
+    }
+
+    // Study Supplies
+    if (id.startsWith('pen') || name.contains('Pen')) return 'Study Supplies';
+    if (id.startsWith('calculator') || name.contains('Calculator')) {
+      return 'Study Supplies';
+    }
+    if (id.startsWith('index-cards') || name.contains('Index Cards')) {
+      return 'Study Supplies';
+    }
+    if (id.startsWith('sticky-notes') || name.contains('Sticky Notes')) {
+      return 'Study Supplies';
+    }
+
+    // Electronics
+    if (id.startsWith('laptop') || name.contains('Laptop')) {
+      return 'Electronics';
+    }
+    if (id.startsWith('headset') || name.contains('Headset')) {
+      return 'Electronics';
+    }
+
+    // Gifts & Memorabilia
     if (id.startsWith('magnet') || name.contains('Magnet')) return 'Gift';
+    if (id.startsWith('mug') || name.contains('Mug')) return 'Gift';
+    if (id.startsWith('frame') || name.contains('Frame')) return 'Gift';
+
     return 'Other';
   }
 
